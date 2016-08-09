ff4175d93f432c2a6c335d3b56d6fd5744f37696
joyc <joy.clark@innoq.com>
2016-08-09 14:54:09 +0200

    sensible defaults for limit & offset

--- src/quackers/quacker.clj
+++ src/quackers/quacker.clj
@@ -6,11 +6,22 @@
             [compojure.core :refer [routes GET POST]]
             [buddy.auth.accessrules :refer [restrict]]))
 
+(defn get-limit [param]
+  (try 
+    (let [i (->int param)]
+      (min i 500))
+    (catch Exception e 10)))
+
+(defn get-offset [param]
+  (try 
+    (->int param)
+    (catch Exception e 0)))
 
 (defn index [request]
-  (let [limit (->int (get-in request [:query-params "limit"] "10"))
-        offset  (->int (get-in request [:query-params "offset"] "0"))
+  (let [limit (get-limit (get-in request [:query-params "limit"]))
+        offset (get-offset (get-in request [:query-params "offset"]))
         quacks (db/get-quacks {:limit limit :offset offset})]
+    (log/info :limit limit :offset offset)
     (h/render request "templates/index.html" {:quacks quacks
                                               :limit  limit
                                               :offset offset
